346c7acf4a3e06c8212b47c666421591ccdbc859
cmoro2002 <cmoro2002@gmail.com>
2023-09-29 10:51:39 +0200

    ToneMapping completo

--- src/ToneMapping.cpp
+++ src/ToneMapping.cpp
@@ -43,9 +43,6 @@ void ToneMapping::ecualizacion() {
         }
     }
 
-    cout << "Minimo: " << min << endl;
-    cout << "Maximo: " << max << endl;
-
     for (int i = 0; i < imagen.getAlto(); i++) {
         for (int j = 0; j < imagen.getAncho(); j++) {
             matriz[i][j] = (matriz[i][j] - min) / (max - min);
@@ -57,24 +54,28 @@ void ToneMapping::ecualizacion() {
 
 // Ecualización hasta el valor v: Transformación lineal de los valores desde el mínimo hasta v.
 void ToneMapping::ecualizacionHastaV(float v) {
-
     float min = 1;
     float max = 0;
-    for (int i = 0; i < imagen.getAlto(); i++) {
-        for (int j = 0; j < imagen.getAncho(); j++) {
-            if (imagen.getMatriz()[i][j] < min) {
-                min = imagen.getMatriz()[i][j];
+    vector<vector<double>> matriz = this->imagen.getMatriz();
+    double alto = this->imagen.getAlto();
+    double ancho = this->imagen.getAncho();
+    for (int i = 0; i < alto; i++) {
+        for (int j = 0; j < ancho * 3; j++) {
+            if (matriz[i][j] < min) {
+                min = matriz[i][j];
             }
-            if (imagen.getMatriz()[i][j] > max) {
-                max = imagen.getMatriz()[i][j];
+            if (matriz[i][j] > max) {
+                max = matriz[i][j];
             }
         }
     }
-    for (int i = 0; i < imagen.getAlto(); i++) {
-        for (int j = 0; j < imagen.getAncho(); j++) {
-            imagen.getMatriz()[i][j] = (imagen.getMatriz()[i][j] - min) / (v - min);
+    for (int i = 0; i < alto; i++) {
+        for (int j = 0; j < ancho * 3; j++) {
+            matriz[i][j] = (matriz[i][j] - min) / (v - min);
         }
     }
+
+    this->imagen.setMatriz(matriz);
 }
 
 // Ecualización y clamp: Combinar los dos anteriores según un parámetro de ”clamping.” Nótese que los dos operadores anteriores pueden considerarse casos particularesde este operador.
@@ -86,11 +87,16 @@ void ToneMapping::ecualizacionClamp(float v) {
 // Curva gamma: Aplicar una curva gamma a todos los valores (necesita ecualización primero).
 void ToneMapping::curvaGamma(float gamma) {
     ecualizacion();
-    for (int i = 0; i < imagen.getAlto(); i++) {
-        for (int j = 0; j < imagen.getAncho(); j++) {
-            imagen.getMatriz()[i][j] = pow(imagen.getMatriz()[i][j], gamma);
+    vector<vector<double>> matriz = this->imagen.getMatriz();
+    double alto = this->imagen.getAlto();
+    double ancho = this->imagen.getAncho();
+    for (int i = 0; i < alto; i++) {
+        for (int j = 0; j < ancho * 3; j++) {
+            matriz[i][j] = pow(matriz[i][j], gamma);
         }
     }
+
+    this->imagen.setMatriz(matriz);
 }
 
 // Clamp y curva gamma: Aplicar una curva gamma después de una operación de clamping (necesita ecualización primero). Nótese que todos los operadores anteriores pueden verse como casos particulares de este operador.
--- tests.cpp
+++ tests.cpp
@@ -129,35 +129,35 @@ int main() {
     cout << "Cargando imagen..." << endl;
     ImagenHDR imagenClamp = imagen;
     ImagenHDR imagenEcualizacion = imagen;
-    // ImagenHDR imagenEcualizacionHastaV = imagen;
-    // ImagenHDR imagenClampEcualizacion = imagen;
-    // ImagenHDR imagenGamma = imagen;
-    // ImagenHDR imagenClampGamma = imagen;
+    ImagenHDR imagenEcualizacionHastaV = imagen;
+    ImagenHDR imagenClampEcualizacion = imagen;
+    ImagenHDR imagenGamma = imagen;
+    ImagenHDR imagenClampGamma = imagen;
 
     cout << "Aplicando tone mapping..." << endl;
     ToneMapping toneMappingClamp(imagenClamp);
     ToneMapping toneMappingEcualizacion(imagenEcualizacion);
-    // ToneMapping toneMappingEcualizacionHastaV(imagenEcualizacionHastaV);
-    // ToneMapping toneMappingClampEcualizacion(imagenClampEcualizacion);
-    // ToneMapping toneMappingGamma(imagenGamma);
-    // ToneMapping toneMappingClampGamma(imagenClampGamma);
+    ToneMapping toneMappingEcualizacionHastaV(imagenEcualizacionHastaV);
+    ToneMapping toneMappingClampEcualizacion(imagenClampEcualizacion);
+    ToneMapping toneMappingGamma(imagenGamma);
+    ToneMapping toneMappingClampGamma(imagenClampGamma);
 
     toneMappingClamp.clamping();
     toneMappingEcualizacion.ecualizacion();
-    // toneMappingEcualizacionHastaV.ecualizacionHastaV(0.5);
-    // toneMappingClampEcualizacion.clamping();
-    // toneMappingClampEcualizacion.ecualizacion();
-    // toneMappingGamma.curvaGamma(2.0);
-    // toneMappingClampGamma.clampCurvaGamma(0.5, 2.0);
+    toneMappingEcualizacionHastaV.ecualizacionHastaV(0.5);
+    toneMappingClampEcualizacion.clamping();
+    toneMappingClampEcualizacion.ecualizacion();
+    toneMappingGamma.curvaGamma(2.0);
+    toneMappingClampGamma.clampCurvaGamma(0.5, 2.0);
 
     // Probar a escribir las imagenes resultantes
     cout << "Escribiendo imagenes..." << endl;
-    escritor.escribirImagenHDR("ppms/bosqueClamp.ppm", imagenClamp);
+    escritor.escribirImagenHDR("ppms/forest_path_clamp.ppm", imagenClamp);
     escritor.escribirImagenHDR("ppms/forest_path_ecualizacion.ppm", imagenEcualizacion);
-    // escritor.escribirImagenHDR("ppms/forest_path_ecualizacionHastaV.ppm", imagenEcualizacionHastaV);
-    // escritor.escribirImagenHDR("ppms/forest_path_clampEcualizacion.ppm", imagenClampEcualizacion);
-    // escritor.escribirImagenHDR("ppms/forest_path_gamma.ppm", imagenGamma);
-    // escritor.escribirImagenHDR("ppms/forest_path_clampGamma.ppm", imagenClampGamma);
+    escritor.escribirImagenHDR("ppms/forest_path_ecualizacionHastaV.ppm", imagenEcualizacionHastaV);
+    escritor.escribirImagenHDR("ppms/forest_path_clampEcualizacion.ppm", imagenClampEcualizacion);
+    escritor.escribirImagenHDR("ppms/forest_path_gamma.ppm", imagenGamma);
+    escritor.escribirImagenHDR("ppms/forest_path_clampGamma.ppm", imagenClampGamma);
 
     std::cout << "Todas las pruebas de tone mapping pasaron con éxito." << std::endl;
 
